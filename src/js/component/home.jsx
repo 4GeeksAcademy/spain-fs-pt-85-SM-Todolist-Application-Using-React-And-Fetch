7640140c83eb64365334f5081652e88047c38358
unduashe <unduashe@outlook.es>
2024-11-24 20:03:31 +0000

    se da funcionalidad al botón para eliminar

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -13,31 +13,31 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //el arr debería de alguna forma ser mapeado y añadido para que cada item sea su propio array que se sea pasado a LiGenerator
 //finalmente habría que utilizar un hook para que pueda cargar la info de forma rápida
 
-
+function test(e){
+	console.log(e.target);
+	
+}
 
 //create your first component
 const Home = () => {
-	const [liItem, setLiItem] = useState(["Coffe"])
+	const [liItem, setLiItem] = useState(["Coffe"]);
 	const liItemHandler = (e) => {
-		if (e.key == "Enter" && e.target.value) {console.log("enter");
+		if (e.key == "Enter" && e.target.value.trim()) {
 		setLiItem(liItem.concat(e.target.value))
 		e.target.value = "";
-		console.log(liItem);
-		
 		}
-	}
-	const liItemGenerator = liItem.map((x,index) =>{
-		return <LiGenerator key={index} item={x} onClick={liItemDeleter} />
-	})
+	};
 
-	const liTargetValue = (e) => e.target.parentNode.childNodes[0].nodeValue;
-	const liItemDeleter = liItem.filter((x,index) => {
-		if (x != liTargetValue)	return <LiGenerator key={index} item={x} onClick={liItemDeleter}/>;
-		else console.log(liTargetValue);
-		
+	const liItemGenerator = liItem.map((x) =>{
+		return <LiGenerator key={x} item={x} onClick={() => itemUpdater(x)} />
 	});
 	
+	const itemUpdater = (valueToRemove) => {
+		const updatedItems = liItem.filter((x) => x != valueToRemove);
+		setLiItem(updatedItems);
+	};
 	
+
 	return (
 		<div className="border">
 			<input type="text" onKeyDown={liItemHandler}/>
