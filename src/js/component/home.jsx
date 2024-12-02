6f257eac89994073568732bba41c6cfeb71d714e
unduashe <unduashe@outlook.es>
2024-12-02 15:46:55 +0000

    ToDo funcional, se añaden cambios de visibisibilidad a botones, se corrije fallo por el que no se cargaban los items del ToDo, se mejora experiencia de usuario y se purgan datos inecesarios

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,10 +3,6 @@ import PropTypes from "prop-types";
 import LiGenerator from "./li";
 import { useState, useEffect } from "react";
 
-
-//que cuando te logues los botones de registrarse y logarte desaparezcan para que aparezca otro de deslogarse
-//REVISAR COMENTARIOS PARA EMPEZAR A DEJARLO BONITO TB, ELIMINAR NAVBAR E INFO INECESARIA DE INDEX.JS
-
 //funcion para dar de alta nuevos usuarios, se le pasa el string del usuario a crear para que haga una llamada POST a la API
 async function createUser(newUser) {
 	try {
@@ -19,7 +15,6 @@ async function createUser(newUser) {
 		return true;
 	}
 	catch (error) {
-		// Error handling
 		console.error(error);
 		return;
 	};
@@ -32,15 +27,9 @@ async function validateUser(userToValidate) {
 		let response = await fetch(`https://playground.4geeks.com/todo/users/${userToValidate}`, {
 			method: 'GET'
 		})
-
-		if (response.status === 200) {
-			console.log(`hola ${userToValidate}`) 
-			return;
-		}
 		if (response.status === 404) {
-			createUser(userToValidate);
-			validateUser(userToValidate)
-			return;
+			await createUser(userToValidate);
+			return await validateUser(userToValidate);
 		}
 		console.log(response);
 		let data = await response.json();
@@ -52,9 +41,6 @@ async function validateUser(userToValidate) {
 		return;
 	}
 }
-//crear otro useEffect el cual cuando vea cambios en userLoggin ejecute la función de compobación de si el usuario existe, en caso de que exista querre que arriba a la izquierda se forme
-//un hola usuario con el valor del usuairo recibido y que en caso de que no exista, se ejecute la función de crear usuario con el valor del usuario que se ha intentado logar para 
-//finalmente añadir el nuevo usuario
 
 //función para hacer post a un nuevo item, recibe el usuario al que se está haciendo el post y un evento para poder obtener así su valor
 async function postNewItem(userToPostNewItem, e) {
@@ -79,24 +65,8 @@ async function postNewItem(userToPostNewItem, e) {
 	}
 }
 
-//función para eliminar elementos de la lista de tareas
-async function itemDeleter(itemToDelete) {
-	try {
-		let response = await fetch(`https://playground.4geeks.com/todo/todos/${itemToDelete.id}`)
-		console.log(response);
-		let data = await response.json();
-		console.log(data);
-		fetchTodos();
-		return
-	} catch (error) {
-		console.log(error);
-		return
-	}
-}
-
-//create your first component //create your first component //create your first component //create your first component //create your first component //create your first component
+//COMPONENTE PRINCIPAL
 const Home = () => {
-	//todo lo de navbar todo lo de navbar todo lo de navbar
 	//constante para el registro de los input de los botones register y login
 	const [inputRegisterNewUser, setInputRegisterNewUser] = useState("");
 	//constante para el registro del nuevo usuario
@@ -111,14 +81,14 @@ const Home = () => {
 	const [registerButtonClassName, setRegisterButtonClassName] = useState("btn btn-info");
 	const [logginButtonClassName, setLogginButtonClassName] = useState("btn btn-primary");
 	//se genera hook para la clase del botón de deslogarse
-	const [logoutButtonClassName, setLogoutButtonClasssName] = useState("btn btn-danger ")
+	const [logoutButtonClassName, setLogoutButtonClasssName] = useState("btn btn-danger d-none")
 
 	//constante para que sea llamada cada vez que el input de los botones register y login sea cambiado añadiendo el valor 
 	const handleInputChange = (e) => {
 		setInputRegisterNewUser(e.target.value);
 	}
 
-	//se genera función sincrona la cual obtiene los todos de la api y lo actualiza en la variable hook
+	//se genera función asincrona la cual obtiene los todos de la api y lo actualiza en la variable hook para ser generados los items en el html a través de otra función
 	async function fetchTodos() {
 		const fetchedTodos = await validateUser(userLoggin)
 		if (fetchedTodos) setTodos(fetchedTodos);
@@ -127,15 +97,20 @@ const Home = () => {
 	//función que toma el valor almacenado provisionalmente en inputRegisterNewUser para almacenarlo en registerNewUser y devolver el valor del input del botón register a un valor vacío
 	const registerNewUserHandler = () => {
 		setRegisterNewUser(inputRegisterNewUser);
+		alert(`Usuario ${inputRegisterNewUser} creado, una vez se haya logado podrá introducir nuevos elementos a su lista de tareas.`)
 		setInputRegisterNewUser("");
 	}
 
 	//función que toma el valor almacenado provisionalmente en inputRegisterNewUser para almacenarlo en userLoggin y devolver el valor del input del botón register a un valor vacío
+	//además de modificar la visibilidad de botones y generar texto de bienvenida
 	const userLogginHandler = async () => {
 		setUserLoggin(inputRegisterNewUser);
 		await fetchTodos();
 		setTextGreeterClassName("fw-bold");
-		alert(`Usuario ${inputRegisterNewUser} logado!`)
+		setRegisterButtonClassName("btn btn-info d-none");
+		setLogginButtonClassName("btn btn-primary d-none");
+		setLogoutButtonClasssName("btn btn-danger");
+		alert(`Usuario ${inputRegisterNewUser} logado!`);
 		setInputRegisterNewUser("");
 	}
 
@@ -148,30 +123,21 @@ const Home = () => {
 	useEffect(() => {
 		validateUser(userLoggin);
 		fetchTodos();
-		//POR AQUI HABRÍA QUE PONER OTRA VARIABLE PARA QUE CAMBIE POR UN LADO LA CLASE DEL NAVBAR HACIENDO QUE SE MUESTRE ARRIBA A LA DERECHA EL USUARIO, PODRÍA ESTAR EN UN PRINCIPIO EN D-NONE
-		//Y QUE CON ESTO SE QUITE ESE DISPLAY
 	}, [userLoggin])
-
-
-
-	//ahora este es el dato que tengo que enviar para la creación de usuario, luego voy a tener que hacer otro para que haga un get del usuario ya creado y que si la repuesta es ok ponga al final
-	//del nabvar un hola y el nombre del usuario que existe
-	console.log(registerNewUser);
-
 
 	//se genera escucha, cuando se pulsa la letra enter y hay un valor distinto a un espacio(tambien quita valores con espacio al inicio y fin del valor introducido)
 	//llama a la función postNewItem del archivo index.js, devuelve el valor del input a un string vacío y llama a la función fetchTodos para actualizar el hook todos
-	const todosHandler = (e) => {
+	const todosHandler = async (e) => {
 		if (!userLoggin){
 			e.preventDefault();
-			alert("Por favor, loguese con su usuario para poder ingresar nuevas tareas.")
+			alert("Por favor, loguese con su usuario para poder modificar este campo.")
 		}
 		if (e.key == "Enter" && e.target.value.trim()) {
 			postNewItem(userLoggin, e)
 			e.target.value = "";
-			fetchTodos();
+			await fetchTodos();
 		}
-		fetchTodos();
+		await fetchTodos()
 	};
 
 	//se hace un map al array todos, el cual genera un LI con el id y label recibidos de la api guardados en el hook todos
@@ -184,10 +150,6 @@ const Home = () => {
 
 	//función para eliminar items de la lista
 	const itemUpdater = async (itemToRemove) => {
-		// console.log(itemToRemove.id);
-		// const updatedItems = todos.filter((x) => x.id != itemToRemove.id);
-		// 	// x != itemToRemove);
-		// setTodos(updatedItems);
 		try {
 			let response = await fetch(`https://playground.4geeks.com/todo/todos/${itemToRemove.id}`,{
 				method:"DELETE"
@@ -201,6 +163,16 @@ const Home = () => {
 		}
 	};
 
+	//función de logout, cierra sesión del usuario activo y maneja la visibilidad de texto de bienvenida y de botones
+	const logOut = async () => {
+	setTextGreeterClassName("d-none");
+	setRegisterButtonClassName("btn btn-info");
+	setLogginButtonClassName("btn btn-primary");
+	setLogoutButtonClasssName("btn btn-danger d-none");
+	setUserLoggin("");
+	setTodos([]);
+	}
+
 	//variable para manejar el texto de los objetos faltantes
 	const todosLeftTextUpdater = todos.length === 1 ? "1 item left" : todos.length + " items left"
 
@@ -212,7 +184,7 @@ const Home = () => {
 						<span className="navbar-toggler-icon"></span>
 					</button>
 					<div className={textGreeterClassName}>Hello {userLoggin}</div>
-					<div className="collapse navbar-collapse d-flex justify-content-end g-3" id="navbarSupportedContent">
+					<div className="collapse navbar-collapse d-flex justify-content-end" id="navbarSupportedContent">
 						<ul className="navbar-nav  mb-2 mb-lg-0 ">
 							{/* botón creación de nuevo usuario */}
 							<li className="nav-item me-3">
@@ -229,11 +201,11 @@ const Home = () => {
 												<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 											</div>
 											<div className="modal-body">
-												Please state the user to create
+												Please state the user to create.
 												<input type="text" className="form-control" value={inputRegisterNewUser} onChange={handleInputChange} />
 											</div>
 											<div className="modal-footer">
-												<button type="button" className="btn btn-primary" onClick={registerNewUserHandler}>Create User</button>
+												<button type="button" className="btn btn-primary" data-bs-dismiss="modal" onClick={registerNewUserHandler}>Create User</button>
 												<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
 											</div>
 										</div>
@@ -257,11 +229,11 @@ const Home = () => {
 												<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 											</div>
 											<div className="modal-body">
-												Please state your user name
+												Please state your user name, if the stated user doesn't exist it will be automatically created and logged in.
 												<input type="text" className="form-control" value={inputRegisterNewUser} onChange={handleInputChange} />
 											</div>
 											<div className="modal-footer">
-												<button type="button" className="btn btn-primary" onClick={userLogginHandler}>Login</button>
+												<button type="button" className="btn btn-primary" data-bs-dismiss="modal" onClick={userLogginHandler}>Login</button>
 												<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
 											</div>
 										</div>
@@ -284,7 +256,7 @@ const Home = () => {
 												<button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 											</div>
 											<div className="modal-footer">
-												<button type="button" className="btn btn-primary" onClick={registerNewUserHandler}>Yes</button>
+												<button type="button" className="btn btn-primary" data-bs-dismiss="modal" onClick={logOut}>Yes</button>
 												<button type="button" className="btn btn-secondary" data-bs-dismiss="modal">No</button>
 											</div>
 										</div>
@@ -296,14 +268,7 @@ const Home = () => {
 					</div>
 				</div>
 			</nav>
-
-
-
-
-
-
-
-
+			{/* fin del navbar */}
 			{/* inicio de cuerpo de la página */}
 			<div className="d-flex justify-content-center fs-1">
 				To Do List
